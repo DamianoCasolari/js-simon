7a0185a232e684dfd4372e7b595584d5023a5df3
DamianoCasolari <damianocasolari@gmail.com>
2023-03-21 20:15:38 +0100

    add win or lose functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,24 +5,68 @@ const containerFirstPart = document.querySelector(".container_random")
 const containerNumber = document.querySelector(".number_random")
 const containerFormEl = document.querySelector(".container_form")
 const timerEl = document.querySelector(".timer")
+const submitEl = document.querySelector("input[type='submit']")
+const inputArray = document.querySelectorAll("input[type='number']")
+const levelContainerEl = document.querySelector(".level_container")
+const levelEl = document.querySelector(".level")
+const forgotEl = document.querySelector(".forgot")
 const randomNumbers = []
+let points_on_five_number = 0;
+
+
+
 
 // I start with a click on my start game button 
-btnEl.addEventListener("click", function() {
+btnEl.addEventListener("click", function () {
     //hide the button and show the container
     btnEl.classList.add("d-none")
     containerCounter.classList.remove("d-none");
+    // correct answer counter reset
+    points_on_five_number = 0;
+    randomNumbers.splice(0,5);
+    timerEl.innerHTML = 5;
+    console.log(points_on_five_number);
+    containerNumber.innerHTML = "";
     // crea 5 numeri randomm 
-for (let index = 0; index < 5; index++) {
-    const singleRandomNumber = Math.floor(Math.random() * 100) + 1;
-    containerNumber.innerHTML += singleRandomNumber + " ";
-    randomNumbers.push(singleRandomNumber)
-    console.log(randomNumbers);
-}
-timerFunction();
-setTimeout(startCountdown,15000 );
+    for (let index = 0; index < 5; index++) {
+        const singleRandomNumber = Math.floor(Math.random() * 100) + 1;
+        containerNumber.innerHTML += singleRandomNumber + " ";
+        randomNumbers.push(singleRandomNumber)
+        // console.log(randomNumbers);
+    }
+    timerFunction();
+    setTimeout(startCountdown, 5000);
 })
 
+
+// add check correct answer to click 
+submitEl.addEventListener("click", function (e) {
+    e.preventDefault();
+    for (let index = 0; index < randomNumbers.length; index++) {
+        const singleRandomNumber = randomNumbers[index];
+        const singleInputNumber = inputArray[index].value
+        if (singleRandomNumber == singleInputNumber) {
+            points_on_five_number++
+
+        } else {
+            levelContainerEl.innerHTML = "You forgot :"
+            levelEl.classList.add("d-none")
+            forgotEl.classList.remove("d-none")
+            forgotEl.innerHTML += singleRandomNumber + " ";
+            if (index == 4) {
+                setTimeout (() => {
+                   if (confirm("Vuoi rigiocare?")) {
+                    location.reload();
+                   }
+                }, 1000)
+            }
+        }
+        checkLevel(points_on_five_number)
+    }
+})
+
+
+// FUNCTION  
 
 function startCountdown() {
     containerFirstPart.classList.add("d-none");
@@ -31,14 +75,26 @@ function startCountdown() {
 }
 
 function timerFunction() {
-    let timeOfTimer = 15;
-    const timerFunction = setInterval(function() { 
-       timeOfTimer--;
-       timerEl.innerHTML = timeOfTimer; 
-       console.log(timeOfTimer);
-       if (timeOfTimer <= 0) {
-        clearInterval(timerFunction);
-      }
+    let timeOfTimer = 5;
+    const timerFunction = setInterval(function () {
+        timeOfTimer--;
+        timerEl.innerHTML = timeOfTimer;
+        //    console.log(timeOfTimer);
+        if (timeOfTimer <= 0) {
+            clearInterval(timerFunction);
+        }
 
-    },1000 ) 
+    }, 1000)
+}
+
+
+function checkLevel(points) {
+    if (points == 5) {
+        levelEl.innerHTML++
+        btnEl.classList.remove("d-none");
+        containerCounter.classList.add("d-none");
+        containerFirstPart.classList.remove("d-none");
+        containerFormEl.classList.add("d-none");
+        btnEl.innerHTML ="Nuovo livello"
+    }
 }
