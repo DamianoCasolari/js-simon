12be531774a5d954fdae7a83eac5d880c50e25a0
DamianoCasolari <damianocasolari@gmail.com>
2023-03-21 18:08:41 +0100

    add a step to show form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 // creo 5 numeri casuali legati ad un add eventlistener
 const btnEl = document.querySelector(".container_random button")
 const containerCounter = document.querySelector(".container_counter")
+const containerFirstPart = document.querySelector(".container_random")
 const containerNumber = document.querySelector(".number_random")
+const containerFormEl = document.querySelector(".container_form")
 const randomNumbers = []
 
 // I start with a click on my start game button 
@@ -16,7 +18,11 @@ for (let index = 0; index < 5; index++) {
     randomNumbers.push(singleRandomNumber)
     console.log(randomNumbers);
 }
-
+setInterval(startCountdown,5000 )
 })
 
+function startCountdown() {
+    containerFirstPart.classList.add("d-none");
+    containerFormEl.classList.remove("d-none");
 
+}
